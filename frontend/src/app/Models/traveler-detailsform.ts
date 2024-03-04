0adc038d320293f869ca722baba0abc4c25b22b3
Shubham <shubhamtambitkar2611@gmail.com>
2024-03-04 12:36:17 +0530

    added form service in service folder

---
+++ frontend/src/app/Models/traveler-detailsform.ts
@@ -0,0 +1,15 @@
+export interface TravelerDetailsform {
+    salutation:string,
+    firstName:string,
+    middleName:string,
+    surName:string,
+    gender:string,
+    dateOfBirth:string,
+    passportNo:string,
+    emailId:string,
+    phoneNumber:string,
+    alternatePhoneNumber:string,
+    addressLine_1:string,
+    addressLine_2:string,
+    addressLine_3:string,
+}
