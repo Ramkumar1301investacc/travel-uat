14692de53b5f67a9dc5cec5e670bc2ad634ec63d
Ramkumar1301investacc <ramkumar@investacc.co.in>
2024-03-08 15:07:32 +0530

    Final phase 1 date 08/03/2024

--- frontend/src/app/pages/form/form.component.ts
+++ frontend/src/app/pages/form/form.component.ts
@@ -1,6 +1,6 @@
 import { FormDataService } from './../../service/form-data.service';
 import { Component, OnInit } from '@angular/core';
-import { NgForm , FormGroup} from '@angular/forms';
+import { NgForm , FormGroup, FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProposalOwnerDetails, ProposalRequest } from 'src/app/Models/FormClasses';
 import { DestAgeNumService } from 'src/app/service/dest-age-num.service';
@@ -21,12 +21,29 @@ export class FormComponent implements OnInit {
     
   stepNumber= 1;
 
-  constructor(private router:Router,private formDataService:FormDataService){
-  }
+  tripDestination = this.getTravelData.destData.tDest;
+  tripStart = this.getTravelData.destData.tripStart;
+  tripEnd = this.getTravelData.destData.tripEnd;
+  singlePlanDetail: any;
+  medicalCoverage: any;
+  passportCoverage: any;
+  baggageCoverage: any;
+  premiumDetail: any;
+  insuranceCost: any;
+  gstCost: any;
+  totalCost: any;
+
+constructor(
+  private router: Router,
+  private fb: FormBuilder,
+  private getTravelData: DestAgeNumService,
+  private getSinglePlanDetail: PlanDetailsService,
+  private formdataservice:FormDataService
+) { }
 
   next(assignedForm:NgForm){
-    this.formDataService.setFormData(this.proposalRequest);
-    console.log("Data is coming from service:",this.formDataService.formData);
+    this.formdataservice.setFormData(this.proposalRequest);
+    console.log("Data is coming from service:",this.formdataservice.formData);
 
     if (this.stepNumber<3) {
       this.stepNumber++
@@ -41,5 +58,20 @@ export class FormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getSinglePlanDetail.getSinglePlanDetails(this.getSinglePlanDetail.singlePlanName).subscribe((response) => {
+      this.singlePlanDetail = response;
+      console.log('In froms component', this.singlePlanDetail);
+      this.medicalCoverage = this.singlePlanDetail.filter((plan: any) => { return plan.pbenefits.includes('Medical') });
+      this.passportCoverage = this.singlePlanDetail.filter((plan: any) => { return plan.pbenefits.includes('Passport') });
+      this.baggageCoverage = this.singlePlanDetail.filter((plan: any) => { return plan.pbenefits.includes('Baggage') });
+  
+      console.log('Medical Coverage', this.medicalCoverage[0].plimits)
+    })
+
+    this.getSinglePlanDetail.getSingleCalculatePremium( 
+      this.getTravelData.getDestData(), 
+      this.getSinglePlanDetail.singlePlanName ). subscribe((response) => {
+        this.premiumDetail = response;
+      })
   }
 }
